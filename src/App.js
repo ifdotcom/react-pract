350af4222ae49a8d3e78007ac65daab3030c2517
Fernanda Vidal <ifervb@gmail.com>
2021-10-25 16:51:56 -0500

    Sintaxis JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let name = "Fernanda";
+  let auth = true;
+  let estaciones = ["primavera", "verano", "otoño", "invierno"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <label htmlFor="nombre">Nombre</label>
+          <input type="text" id="nombre" />
+          <h1>{name}</h1>
+          <p>{auth ? "Usuario logueado" : "Usuario no logueado"}</p>
+          <p>{2 + 2}</p>
+          <ul>
+            {estaciones.map((el, index) => (
+              <li key={index}>{el}</li>
+            ))}
+          </ul>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <div></div>
+    </>
   );
 }
 
